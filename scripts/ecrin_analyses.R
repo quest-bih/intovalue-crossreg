d101ab21552a35eba218af0b832ee4354f045888
Luciano Fernandez <lucianofernandez2018@gmail.com>
2024-10-25 17:03:03 +0200

    Initial commit

---
+++ scripts/ecrin_analyses.R
@@ -0,0 +1,162 @@
+# A script to evaluate the degree of overlap between ECRIN MDR cross registrations
+# and the cross registrations we found
+
+library(tidyverse)
+library(VennDiagram)
+library(here)
+library(lubridate)
+library(here)
+library(fs)
+library(lubridate)
+library(stringr)
+library(ctregistries)
+library(cli)
+
+# Function to standardize trial pairs, give them unique identifier
+standardize_pairs <- function(df) {
+  df |>
+    rowwise() |>
+    mutate(
+      # Sort trial_id_1 and trial_id_2 alphabetically within each row to handle the order issue
+      standardized_pair = paste(sort(c(trn1, trn2)), collapse = "_")
+    ) |>
+    ungroup()
+}
+
+
+# Load TRN manual checks first
+trn_manual_checks <- read_rds("data/trn_manual_checks.rds")
+trn_filtered <- trn_manual_checks |>
+  filter(priority <= 4 )
+
+# Filter TRN for EUCTR and DRKS that resolves
+trn_resolves <- trn_filtered |>
+  filter(drks_removed == FALSE & euctr_id_in_euctr == TRUE) |>
+  filter(trn2 != "2008-004408-29")
+
+# read in ECRIN information
+ecrin <- read_csv("data/raw/20240916_mdr_ids_euctr.csv") |>
+  rename(trn1 = iv_id,
+         trn2 = identifier)
+
+
+# Apply the function to both datasets (do NOT filter for trials that resolve in TRN)
+ecrin_standardized <- standardize_pairs(ecrin)
+trn_standardized <- standardize_pairs(trn_filtered)
+
+# trn_resolves_standardized <- standardize_pairs(trn_resolves)
+
+# Select only the relevant column containing the standardized pairs
+ecrin_pairs <- ecrin_standardized |> select(standardized_pair)
+trn_pairs <- trn_standardized |> select(standardized_pair)
+
+# trn_resolves_pairs <- trn_resolves_standardized |> select(standardized_pair)
+
+# Convert to vectors for easy comparison
+ecrin_pairs_vector <- ecrin_pairs$standardized_pair
+trn_pairs_vector <- trn_pairs$standardized_pair
+
+# trn_resolves_pairs_vector <- trn_resolves_pairs$standardized_pair
+
+# Create a Venn diagram for the pair overlaps
+overlap_plot <- venn.diagram(
+  x = list(
+    "ECRIN" = ecrin_pairs_vector,
+    "TRN Manual" = trn_pairs_vector
+  ),
+  category.names = c("ECRIN", "TRN Manual"),
+  filename = NULL,  # Don't save to file; display directly
+  output = TRUE,
+  fill = c("lightblue", "lightgreen"),
+  alpha = 0.5,
+  cex = 2,
+  fontfamily = "sans",
+  cat.fontfamily = "sans",
+  cat.cex = 2,
+  cat.pos = c(-20, 20)
+)
+
+# Plot the Venn diagram
+grid.draw(overlap_plot)
+
+# Create a Venn diagram for the pair overlaps for TRNs that resolve
+resolves_overlap_plot <- venn.diagram(
+  x = list(
+    "ECRIN" = ecrin_pairs_vector,
+    "TRN Manual" = trn_resolves_pairs_vector
+  ),
+  category.names = c("ECRIN", "TRN Manual"),
+  filename = NULL,  # Don't save to file; display directly
+  output = TRUE,
+  fill = c("lightblue", "lightgreen"),
+  alpha = 0.5,
+  cex = 2,
+  fontfamily = "sans",
+  cat.fontfamily = "sans",
+  cat.cex = 2,
+  cat.pos = c(-20, 20)
+)
+
+# Plot the Venn diagram
+grid.draw(resolves_overlap_plot)
+
+common_pairs <- intersect(ecrin_pairs_vector, trn_pairs_vector)
+unique_to_ecrin <- setdiff(ecrin_pairs_vector, trn_pairs_vector)
+unique_to_trn <- setdiff(trn_pairs_vector, ecrin_pairs_vector)
+
+# Pairs that resolve
+# resolves_common_pairs <- intersect(ecrin_pairs_vector, trn_resolves_pairs_vector)
+# resolves_unique_to_ecrin <- setdiff(ecrin_pairs_vector, trn_resolves_pairs_vector)
+# resolves_unique_to_trn <- setdiff(trn_resolves_pairs_vector, ecrin_pairs_vector)
+
+
+# Summary counts
+total_ecrin <- length(ecrin_pairs_vector)
+total_trn <- length(trn_pairs_vector)
+common_count <- length(common_pairs)
+unique_to_ecrin_count <- length(unique_to_ecrin)
+unique_to_trn_count <- length(unique_to_trn)
+
+# Summary counts for resolving pairs
+resolves_total_ecrin <- length(ecrin_pairs_vector)
+resolves_total_trn <- length(trn_resolves_pairs_vector)
+resolves_common_count <- length(resolves_common_pairs)
+resolves_unique_to_ecrin_count <- length(resolves_unique_to_ecrin)
+resolves_unique_to_trn_count <- length(resolves_unique_to_trn)
+
+# tables of trial pairs unique to ecrin or TRN
+unique_to_ecrin_df <- ecrin_pairs[!ecrin_pairs$standardized_pair %in% trn_pairs$standardized_pair, ]
+unique_to_trn_df <- trn_pairs[!trn_pairs$standardized_pair %in% ecrin_pairs$standardized_pair, ]
+
+# add unique flag
+unique_to_trn_df <- unique_to_trn_df |>
+  mutate(unique_to_trn = 1)
+
+# identify unique to trn pairs in broader table
+trn_unique_flagged <- trn_standardized |>
+  left_join(unique_to_trn_df, by = "standardized_pair") |>
+  filter(unique_to_trn == 1)
+
+# Filter by first two priorities
+# trn_unique_priority_1_2 <- trn_unique_flagged |>
+#  filter(priority <= 2)
+
+
+# Find the rows in `trn_pairs` that are unique to trn (not present in ecrin)
+# resolves_unique_to_trn_df <- trn_resolves_pairs[!trn_resolves_pairs$standardized_pair %in% ecrin_pairs$standardized_pair, ]
+
+
+# Adding back registry columns to filtered table
+# trn_filtered <- trn_filtered |>
+#  rowwise()|>
+#  mutate(registry1 = which_registry(trn1),
+#         registry2 = which_registry(trn2))
+
+# trn_resolves <- trn_resolves |>
+#  rowwise()|>
+#  mutate(registry1 = which_registry(trn1),
+#         registry2 = which_registry(trn2))
+
+# trn_resolves <- trn_resolves |>
+#  filter(trn2 != "2008-004408-29")
+
---
+++ scripts/upset_analysis.R
@@ -0,0 +1,248 @@
+# Script to make upset plots of different ways we identified crossregs
+library(tidyverse)
+library(here)
+library(lubridate)
+library(here)
+library(fs)
+library(lubridate)
+library(stringr)
+library(ctregistries)
+library(cli)
+library(ggupset)
+library(ggplot2)
+
+# Read in manual checks table, filter out everything w/ priority more than 4 and non-resolving or removed TRNs
+trn_manual_checks = read_rds("data/trn_manual_checks.rds")
+
+trn_filtered <- trn_manual_checks |>
+  filter(priority <= 4) |>
+  filter(drks_removed == FALSE & euctr_id_in_euctr == TRUE) |>
+  filter(trn2 != "2008-004408-29")
+
+# Add registries back in
+trn_filtered <- trn_filtered |>
+  rowwise()|>
+  mutate(registry1 = which_registry(trn1),
+         registry2 = which_registry(trn2))
+
+# Add columns to simplify identifying bidirectional/unidirectional linking, make upset plot easier to build
+# Add column to identify non-EUCTR registry
+trn_filtered <- trn_filtered |>
+  mutate(
+    bidirectional = if_else(trn1inreg2 & trn2inreg1, TRUE, FALSE),
+    unidirectional = if_else(trn1inreg2 | trn2inreg1, TRUE, FALSE),
+    non_euctr_registry = ifelse(registry1 == "EudraCT", registry2, registry1)
+  )
+
+#########################################################################################################################
+# Count and analyze how many cross-registrations are in all 3 registrations (informal) analysis
+
+# Assign euctr_trn and non_euctr_trn based on the registry
+trn_filtered <- trn_filtered |>
+  mutate(
+    euctr_trn = ifelse(registry1 == "EudraCT", trn1, ifelse(registry2 == "EudraCT", trn2, NA)),
+    non_euctr_trn = ifelse(registry1 != "EudraCT", trn1, trn2),
+    non_euctr_registry = ifelse(registry1 != "EudraCT", registry1, registry2)
+  )
+
+# Find EUCTR trials linked to multiple non-EUCTR TRNs
+duplicates <- trn_filtered |>
+  group_by(euctr_trn) |>
+  filter(n_distinct(non_euctr_trn) > 1) |>  # Keep only EUCTR IDs linked to more than one non-EUCTR ID
+  ungroup()
+
+# Returns 5 pairs
+# DRKS00000582 - NCT00989352 (linked by 2008-007645-31) - does appear like a valid crossreg
+# NCT02035709 - NCT01490268 (found by us in Priority 7, linked by 2011-003648-31, also NCT02035709 is also linked to 2013-002875-16 in Priority 2) - UNSURE if valid crossreg
+# NCT01703832 - NCT01703819 ( NCT01703832 also connected to 2012-002359-40 in Priority 1, these two connected by 2012-002358-22 ) - These look like identical trials
+# NCT02309918 - DRKS00006863 (not seen previously, connected by 2013-001081-42) - Looks VALID
+# DRKS00003170 - NCT01387048 (not seen previously, connected by 2011-000152-42) - looks VALID
+
+# also potential linkages within EUCTR?
+# 2011-003648-31 - 2013-002875-16 (2013 link no longer resolves)
+# 2012-002359-40 - 2012-002358-22 (first one is open label, second is double-blind, otherwise the same)
+
+# More in depth description in Paper Outline document
+
+#########################################################################################################################
+# Continue making upset plot:
+
+# Make trn_filtered readable for ggupset package
+trn_combos <-
+  trn_filtered |>
+  select(trn1,
+         trn2,
+         non_euctr_registry,
+         is_title_matched,
+         at_least_one_pub,
+         bidirectional,
+         unidirectional
+  ) |>
+  rename(
+    "Title matched" = is_title_matched,
+    "Publication link" = at_least_one_pub,
+    "Bidirectional link" = bidirectional,
+    "Undirectional link" = unidirectional
+  ) |>
+  pivot_longer(cols = -c(trn1, trn2, non_euctr_registry), names_to = "link") |>
+  filter(value == TRUE) |>
+  group_by(trn1, trn2) |>
+  mutate(links = list(link)) |>
+  ungroup() |>
+  select(-value, -link) |>
+  distinct()
+
+# Filter for only DRKS analysis
+trn_combos_drks <- trn_combos |>
+  filter(non_euctr_registry == "DRKS")
+
+# Filter for only CTgov analysis
+trn_combos_ctgov <- trn_combos |>
+  filter(non_euctr_registry == "ClinicalTrials.gov")
+
+# Upset plot showing all TRN pairs in analysis set
+overall_crossreg_combinations <- trn_combos |>
+  ggplot(aes(x=links)) +
+  geom_bar() +
+  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1) +
+  scale_x_upset(n_intersections = 20) +
+  ylab("Number of pairs") +
+  xlab("Linking combinations") +
+  theme(
+    legend.background = element_rect(color = "transparent", fill = "transparent"),
+    legend.position.inside = c(.85, .9),
+    axis.title.y = element_text(size = 11)
+  )
+
+# Upset plot showing all TRN pairs as proportions
+overall_crossreg_combinations_proportions <- trn_combos |>
+  ggplot(aes(x = links)) +
+  geom_bar(aes(y = after_stat(count / 625 * 100)), position = 'stack', fill = "steelblue") +  # Convert to percentage
+  geom_text(stat = 'count', aes(label = sprintf("%.1f%%", after_stat(count / 625 * 100))), vjust = -1) + # Label as percentage
+  scale_x_upset(n_intersections = 20) +
+  scale_y_continuous(limits = c(0, 50)) +
+  ylab("Proportion of pairs (%)") +
+  xlab("Linking combinations")
+
+
+# Upset plot showing all TRN pairs in analysis set, divided by registry (not super readable yet, too much text)
+registry_divided_combinations <- trn_combos |>
+  ggplot(aes(x=links)) +
+  facet_wrap(~non_euctr_registry) +
+  geom_bar() +
+  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1) +
+  scale_x_upset(n_intersections = 20) +
+  ylab("Number of pairs") +
+  xlab("Linking combinations") +
+  ggupset::theme_combmatrix(
+    combmatrix.panel.line.size = 0,
+    combmatrix.label.text = element_text(family = "Roboto", size = 11)
+  ) +
+  theme(
+    legend.background = element_rect(color = "transparent", fill = "transparent"),
+    legend.position = c(.85, .9),
+    axis.title.y = element_text(size = 11),
+    # Remove all x-axis text
+    axis.text.x = element_blank(),
+    # Remove x-axis ticks
+    axis.ticks.x = element_blank()
+  )
+
+# Upset plot of TRN pairs between DRKS and EUCTR
+drks_crossreg_combinations <- trn_combos_drks |>
+  ggplot(aes(x=links)) +
+  geom_bar() +
+  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1) +
+  scale_x_upset(n_intersections = 20) +
+  ylab("Number of pairs") +
+  xlab("Linking combinations") +
+  theme(
+    legend.background = element_rect(color = "transparent", fill = "transparent"),
+    legend.position.inside = c(.85, .9),
+    axis.title.y = element_text(size = 11)
+  )
+
+#  Upset plot of TRN pairs between DRKS and EUCTR (Proportions instead of counts)
+drks_crossreg_combinations_proportions <- trn_combos_drks |>
+  ggplot(aes(x = links)) +
+  geom_bar(aes(y = after_stat(count / 62 * 100)), position = 'stack', fill = "steelblue") +  # Convert to percentage
+  geom_text(stat = 'count', aes(label = sprintf("%.1f%%", after_stat(count / 62 * 100))), vjust = -1) + # Label as percentage
+  scale_x_upset(n_intersections = 20) +
+  scale_y_continuous(limits = c(0, 50)) +
+  ylab("Proportion of pairs (%)") +
+  xlab("Linking combinations")
+
+
+# Upset plot of TRN pairs between CTgov and EUCTR
+ctgov_crossreg_combinations <- trn_combos_ctgov |>
+  ggplot(aes(x=links)) +
+  geom_bar() +
+  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1) +
+  scale_x_upset(n_intersections = 20) +
+  ylab("Number of pairs") +
+  xlab("Linking combinations") +
+  theme(
+    legend.background = element_rect(color = "transparent", fill = "transparent"),
+    legend.position.inside = c(.85, .9),
+    axis.title.y = element_text(size = 11)
+  )
+
+# Upset plot of TRN pairs between CT and EUCTR (Proportions instead of counts)
+ctgov_crossreg_combinations_proportions <- trn_combos_ctgov |>
+  ggplot(aes(x = links)) +
+  geom_bar(aes(y = after_stat(count / 563 * 100)), position = 'stack', fill = "steelblue") +  # Convert to percentage
+  geom_text(stat = 'count', aes(label = sprintf("%.1f%%", after_stat(count / 563 * 100))), vjust = -1) + # Label as percentage
+  scale_x_upset(n_intersections = 20) +
+  scale_y_continuous(limits = c(0, 50)) +
+  ylab("Proportion of pairs (%)") +
+  xlab("Linking combinations")
+
+# # Move y axis label closer to plot
+# # Thanks to https://stackoverflow.com/questions/68593982
+# plot_upset_links_reg_pub_registry +
+#   theme(axis.title.y=element_blank()) +
+#   annotate(geom = "text", x = -0.2, y = 6500, label = "count", angle = 90, size=4) +
+#   coord_cartesian(xlim = c(1, 8), clip = "off")
+
+# `ggupset` doesn't currently allow to change label order
+# https://github.com/const-ae/ggupset/issues/20
+
+# SAVE UPSET PLOTS AS PDFs
+# For now, not sure what we will need later
+
+ggsave(
+  "overall_crossreg_combinations.pdf",
+  overall_crossreg_combinations,
+  scale = 1.25,
+  width = 7,
+  height = 5
+  # scale = 2
+)
+
+ggsave(
+  "registry_divided_combinations.pdf",
+  registry_divided_combinations,
+  scale = 1.25,
+  width = 7,
+  height = 5
+  # scale = 2
+)
+
+ggsave(
+  "drks_crossreg_combinations.pdf",
+  drks_crossreg_combinations,
+  scale = 1.25,
+  width = 7,
+  height = 5
+  # scale = 2
+)
+
+ggsave(
+  "ctgov_crossreg_combinations.pdf",
+  ctgov_crossreg_combinations,
+  scale = 1.25,
+  width = 7,
+  height = 5
+  # scale = 2
+)
+
