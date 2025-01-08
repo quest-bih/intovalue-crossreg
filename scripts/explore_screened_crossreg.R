6fd0d8a14473660169decf7ca0deaf562005eef7
Luciano Fernandez <lucianofernandez2018@gmail.com>
2025-01-08 16:57:26 -0500

    Reorganize order of tables, comment out code for last two tables

--- scripts/explore_screened_crossreg.R
+++ scripts/explore_screened_crossreg.R
@@ -102,186 +102,6 @@ trn_filtered <- trn_filtered |>
 # Filter for trials linked by abstract mention
 #pub_abs <- pub_crossregs |>
 #  filter(trn1_in_pub_abs | trn2_in_pub_abs)
-################################################################################
-# Investigate how false positive (screened, but not confirmed) cross-registrations are connected ( 8/9 false positives are Category 4, so this will be investigated here)
-
-# Filter for false cross-registrations
-false_crossreg_standardized <- manual_validated_standardized |>
-  filter(!is_true_crossreg) |>
-  select(standardized_pair, is_true_crossreg)
-
-false_crossreg <- false_crossreg_standardized |>
-  left_join(trn_filtered, by = "standardized_pair")
-
-# Visual inspection of table shows that all 8 are only connected by trn2_in_pub_ft
-# In this case, 8 EUCTR numbers were mentioned in the full text of publications associated with 8 IntoValue TRNs
-
-################################################################################
-# Upset plot for trials linked by publication
-# The plot here is for trial pairs linked by publications that were screened and CONFIRMED as true cross-registrations
-# The other publication upset also includes pairs that were not true cross-registrations
-# This plot is deprecated and has been commented out here
-
-
-#Change format to make friendlier for ggupset
-#upset_pub_crossregs <-
-#  pub_crossregs |>
-#  select(trn1,
-#         trn2,
-#         trn_in_pub_si,
-#         trn_in_pub_abs,
-#         trn_in_pub_ft,
-#  ) |>
-#  rename(
-#    "trn in SI" = trn_in_pub_si,
-#    "trn in abstract" = trn_in_pub_abs,
-#    "trn in full text" = trn_in_pub_ft,
-#  ) |>
-#  pivot_longer(cols = -c(trn1, trn2), names_to = "link") |>
-#  filter(value == TRUE) |>
-#  group_by(trn1, trn2) |>
-#  mutate(links = list(link)) |>
-#  ungroup() |>
-#  select(-value, -link) |>
-#  distinct()
-
-# Plot upset
-#pub_linking_combinations <- upset_pub_crossregs |>
-#  ggplot(aes(x=links)) +
-#  geom_bar() +
-#  ggtitle("Overall Combinations") +
-#  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1) +
-#  scale_x_upset(n_intersections = 20) +
-#  ylab("Number of pairs") +
-#  xlab("Linking combinations") +
-#  theme(
-#    legend.background = element_rect(color = "transparent", fill = "transparent"),
-#    legend.position.inside = c(.85, .9),
-#    axis.title.y = element_text(size = 11)
-#  )
-
-################################################################################
-# Upset plot for all trials that were screened and CONFIRMED, across all links (not just publications)
-
-# Full table containing only screened and confirmed crossregs
-validated_crossreg <- trn_filtered |>
-  left_join(manual_validated_standardized, by = "standardized_pair") |> 
-  mutate(is_true_crossreg = ifelse(standardized_pair == "2010-023688-16_NCT01326767", TRUE, is_true_crossreg))|> # Manually change is_true_crossreg back to TRUE for row "2010-023688-16_NCT01326767", not sure why it changes at all
-  filter(is_true_crossreg)
-
-# Make validated_crossreg readable for ggupset package 
-upset_validated_crossreg <- validated_crossreg |>
-  select(trn1,
-         trn2,
-         non_euctr_registry,
-         is_title_matched,
-         at_least_one_pub,
-         bidirectional,
-         unidirectional
-  ) |>
-  rename(
-    "Title matched" = is_title_matched,
-    "Publication link" = at_least_one_pub,
-    "Bidirectional link" = bidirectional,
-    "Unidirectional link" = unidirectional
-  ) |>
-  pivot_longer(cols = -c(trn1, trn2, non_euctr_registry), names_to = "link") |>
-  filter(value == TRUE) |>
-  group_by(trn1, trn2) |>
-  mutate(links = list(link)) |>
-  ungroup() |>
-  select(-value, -link) |>
-  distinct()
-
-# Upset plot showing all screened and CONFIRMED TRN pairs as proportions
-validated_crossreg_combinations_proportions <- upset_validated_crossreg |>
-  ggplot(aes(x = links)) +
-  geom_bar(aes(y = after_stat(count / nrow(upset_validated_crossreg) * 100))) +  # Set y as proportion for correct scaling
-  ggtitle("Confirmed Crossreg Combinations Proportions") +
-  geom_text(stat = 'count', aes(y = after_stat(count / nrow(upset_validated_crossreg) * 100), label = sprintf("%.1f%%", after_stat(count / nrow(upset_validated_crossreg) * 100))), vjust = -1) + # Display as percentages
-  scale_x_upset(n_intersections = 20) +
-  scale_y_continuous(limits = c(0, 30), expand = expansion(mult = c(0, 0.05))) +  # Adjust y-axis limits and add small padding
-  ylab("Proportion of pairs (%)") +  
-  xlab("Linking combinations") +
-  theme(
-    legend.background = element_rect(color = "transparent", fill = "transparent"),
-    legend.position = c(.85, .9),
-    axis.title.y = element_text(size = 11)
-  )
-
-
-################################################################################
-# Upset plot for screened trials linked by publication, with false positive count
-
-# Add information about true/false positivity on cross registration status to `trn_filtered`
-manual_validated<- manual_validated_standardized |>
-  left_join(trn_filtered, by = "standardized_pair") |>
-  mutate(is_true_crossreg = ifelse(standardized_pair == "2010-023688-16_NCT01326767", TRUE, is_true_crossreg)) # Manually change is_true_crossreg back to TRUE for row "2010-023688-16_NCT01326767", not sure why it changes at all
-
-# Filter for trials linked by publication in any way
-pub_crossregs_manual_validated <- manual_validated |>
-  filter(at_least_one_pub) |>
-  mutate(
-    trn1_in_pub_abs = replace_na(trn1_in_pub_abs, FALSE),
-    trn2_in_pub_abs = replace_na(trn2_in_pub_abs, FALSE),
-    trn_in_pub_abs = trn1_in_pub_abs | trn2_in_pub_abs,
-    
-    trn1_in_pub_si = replace_na(trn1_in_pub_si, FALSE),
-    trn2_in_pub_si = replace_na(trn2_in_pub_si, FALSE),
-    trn_in_pub_si = trn1_in_pub_si | trn2_in_pub_si,
-    
-    trn1_in_pub_ft = replace_na(trn1_in_pub_ft, FALSE),
-    trn2_in_pub_ft = replace_na(trn2_in_pub_ft, FALSE),
-    trn_in_pub_ft = trn1_in_pub_ft | trn2_in_pub_ft
-  )
-
-
-#Change format to make friendlier for ggupset
-upset_manual_validation <-
-  pub_crossregs_manual_validated |>
-  select(trn1,
-         trn2,
-         trn_in_pub_si,
-         trn_in_pub_abs,
-         trn_in_pub_ft,
-         standardized_pair
-  ) |>
-  rename(
-    "trn in SI" = trn_in_pub_si,
-    "trn in abstract" = trn_in_pub_abs,
-    "trn in full text" = trn_in_pub_ft,
-  ) |>
-  pivot_longer(cols = -c(trn1, trn2, standardized_pair), names_to = "link") |>
-  filter(value == TRUE) |>
-  group_by(trn1, trn2) |>
-  mutate(links = list(link)) |>
-  ungroup() |>
-  select(-value, -link) |>
-  distinct()
-
-upset_manual_false_positive <- upset_manual_validation |>
-  left_join(manual_validated_standardized, by = "standardized_pair")
-
-# Plot false positives in pub linkages
-  
-pub_linking_combinations_false_positive <- upset_manual_false_positive |> 
-  ggplot(aes(x = links, fill = is_true_crossreg)) + 
-  geom_bar(position = "stack") + 
-  geom_text(
-    stat = "count", 
-    aes(label = after_stat(count), group = is_true_crossreg), 
-    position = position_stack(vjust = 0.5) # Place labels in the middle of each section
-  ) + 
-  scale_x_upset(n_intersections = 20) + 
-  ylab("Number of pairs") + 
-  xlab("Linking combinations") + 
-  scale_fill_manual(values = c("TRUE" = "#B3EBF2", "FALSE" = "#009E73"),
-                    name = "Confirmed as a true cross-registration") +
-  theme(
-    legend.background = element_rect(color = "transparent", fill = "transparent"), 
-    legend.position = c(.85, .9),
-    axis.title.y = element_text(size = 11)
-  )
 
 ############################################################################
 # Upset plot for screened TRN pairs (with all categories, not just publication linkages)
@@ -342,3 +162,187 @@ manual_validation_plot <- manual_validation_upset |>
 
 
 ############################################################################
+# Upset plot for screened trials linked by publication, with false positive count
+
+# Add information about true/false positivity on cross registration status to `trn_filtered`
+manual_validated<- manual_validated_standardized |>
+  left_join(trn_filtered, by = "standardized_pair") |>
+  mutate(is_true_crossreg = ifelse(standardized_pair == "2010-023688-16_NCT01326767", TRUE, is_true_crossreg)) # Manually change is_true_crossreg back to TRUE for row "2010-023688-16_NCT01326767", not sure why it changes at all
+
+# Filter for trials linked by publication in any way
+pub_crossregs_manual_validated <- manual_validated |>
+  filter(at_least_one_pub) |>
+  mutate(
+    trn1_in_pub_abs = replace_na(trn1_in_pub_abs, FALSE),
+    trn2_in_pub_abs = replace_na(trn2_in_pub_abs, FALSE),
+    trn_in_pub_abs = trn1_in_pub_abs | trn2_in_pub_abs,
+    
+    trn1_in_pub_si = replace_na(trn1_in_pub_si, FALSE),
+    trn2_in_pub_si = replace_na(trn2_in_pub_si, FALSE),
+    trn_in_pub_si = trn1_in_pub_si | trn2_in_pub_si,
+    
+    trn1_in_pub_ft = replace_na(trn1_in_pub_ft, FALSE),
+    trn2_in_pub_ft = replace_na(trn2_in_pub_ft, FALSE),
+    trn_in_pub_ft = trn1_in_pub_ft | trn2_in_pub_ft
+  )
+
+
+#Change format to make friendlier for ggupset
+upset_manual_validation <-
+  pub_crossregs_manual_validated |>
+  select(trn1,
+         trn2,
+         trn_in_pub_si,
+         trn_in_pub_abs,
+         trn_in_pub_ft,
+         standardized_pair
+  ) |>
+  rename(
+    "trn in SI" = trn_in_pub_si,
+    "trn in abstract" = trn_in_pub_abs,
+    "trn in full text" = trn_in_pub_ft,
+  ) |>
+  pivot_longer(cols = -c(trn1, trn2, standardized_pair), names_to = "link") |>
+  filter(value == TRUE) |>
+  group_by(trn1, trn2) |>
+  mutate(links = list(link)) |>
+  ungroup() |>
+  select(-value, -link) |>
+  distinct()
+
+upset_manual_false_positive <- upset_manual_validation |>
+  left_join(manual_validated_standardized, by = "standardized_pair")
+
+# Plot false positives in pub linkages
+
+pub_linking_combinations_false_positive <- upset_manual_false_positive |> 
+  ggplot(aes(x = links, fill = is_true_crossreg)) + 
+  geom_bar(position = "stack") + 
+  geom_text(
+    stat = "count", 
+    aes(label = after_stat(count), group = is_true_crossreg), 
+    position = position_stack(vjust = 0.5) # Place labels in the middle of each section
+  ) + 
+  scale_x_upset(n_intersections = 20) + 
+  ylab("Number of pairs") + 
+  xlab("Linking combinations") + 
+  scale_fill_manual(values = c("TRUE" = "#B3EBF2", "FALSE" = "#009E73"),
+                    name = "Confirmed as a true cross-registration") +
+  theme(
+    legend.background = element_rect(color = "transparent", fill = "transparent"), 
+    legend.position = c(.85, .9),
+    axis.title.y = element_text(size = 11)
+  )
+
+################################################################################
+
+# Investigate how false positive (screened, but not confirmed) cross-registrations are connected ( 8/9 false positives are Category 4, so this will be investigated here)
+
+# Filter for false cross-registrations
+false_crossreg_standardized <- manual_validated_standardized |>
+  filter(!is_true_crossreg) |>
+  select(standardized_pair, is_true_crossreg)
+
+false_crossreg <- false_crossreg_standardized |>
+  left_join(trn_filtered, by = "standardized_pair")
+
+# Visual inspection of table shows that all 8 are only connected by trn2_in_pub_ft
+# In this case, 8 EUCTR numbers were mentioned in the full text of publications associated with 8 IntoValue TRNs
+
+################################################################################
+# Upset plot for all trials that were screened and CONFIRMED, across all links (not just publications)
+# This code will likely not be used for any table in the paper and is thus deprecated
+
+# Full table containing only screened and confirmed crossregs
+#validated_crossreg <- trn_filtered |>
+#  left_join(manual_validated_standardized, by = "standardized_pair") |> 
+#  mutate(is_true_crossreg = ifelse(standardized_pair == "2010-023688-16_NCT01326767", TRUE, is_true_crossreg))|> # Manually change is_true_crossreg back to TRUE for row "2010-023688-16_NCT01326767", not sure why it changes at all
+#  filter(is_true_crossreg)
+
+# Make validated_crossreg readable for ggupset package 
+#upset_validated_crossreg <- validated_crossreg |>
+#  select(trn1,
+#         trn2,
+#         non_euctr_registry,
+#         is_title_matched,
+#         at_least_one_pub,
+#         bidirectional,
+#         unidirectional
+#  ) |>
+#  rename(
+#    "Title matched" = is_title_matched,
+#    "Publication link" = at_least_one_pub,
+#    "Bidirectional link" = bidirectional,
+#    "Unidirectional link" = unidirectional
+#  ) |>
+#  pivot_longer(cols = -c(trn1, trn2, non_euctr_registry), names_to = "link") |>
+#  filter(value == TRUE) |>
+#  group_by(trn1, trn2) |>
+#  mutate(links = list(link)) |>
+#  ungroup() |>
+#  select(-value, -link) |>
+#  distinct()
+
+# Upset plot showing all screened and CONFIRMED TRN pairs as proportions
+#validated_crossreg_combinations_proportions <- upset_validated_crossreg |>
+#  ggplot(aes(x = links)) +
+#  geom_bar(aes(y = after_stat(count / nrow(upset_validated_crossreg) * 100))) +  # Set y as proportion for correct scaling
+#  ggtitle("Confirmed Crossreg Combinations Proportions") +
+#  geom_text(stat = 'count', aes(y = after_stat(count / nrow(upset_validated_crossreg) * 100), label = sprintf("%.1f%%", after_stat(count / nrow(upset_validated_crossreg) * 100))), vjust = -1) + # Display as percentages
+# scale_x_upset(n_intersections = 20) +
+#  scale_y_continuous(limits = c(0, 30), expand = expansion(mult = c(0, 0.05))) +  # Adjust y-axis limits and add small padding
+#  ylab("Proportion of pairs (%)") +  
+#  xlab("Linking combinations") +
+#  theme(
+   # legend.background = element_rect(color = "transparent", fill = "transparent"),
+  #  legend.position = c(.85, .9),
+ #   axis.title.y = element_text(size = 11)
+#  )
+
+
+################################################################################
+# Upset plot for trials linked by publication
+# The plot here is for trial pairs linked by publications that were screened and CONFIRMED as true cross-registrations
+# The other publication upset also includes pairs that were not true cross-registrations
+# This plot is deprecated and has been commented out here
+
+
+#Change format to make friendlier for ggupset
+#upset_pub_crossregs <-
+#  pub_crossregs |>
+#  select(trn1,
+#         trn2,
+#         trn_in_pub_si,
+#         trn_in_pub_abs,
+#         trn_in_pub_ft,
+#  ) |>
+#  rename(
+#    "trn in SI" = trn_in_pub_si,
+#    "trn in abstract" = trn_in_pub_abs,
+#    "trn in full text" = trn_in_pub_ft,
+#  ) |>
+#  pivot_longer(cols = -c(trn1, trn2), names_to = "link") |>
+#  filter(value == TRUE) |>
+#  group_by(trn1, trn2) |>
+#  mutate(links = list(link)) |>
+#  ungroup() |>
+#  select(-value, -link) |>
+#  distinct()
+
+# Plot upset
+#pub_linking_combinations <- upset_pub_crossregs |>
+#  ggplot(aes(x=links)) +
+#  geom_bar() +
+#  ggtitle("Overall Combinations") +
+#  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1) +
+#  scale_x_upset(n_intersections = 20) +
+#  ylab("Number of pairs") +
+#  xlab("Linking combinations") +
+#  theme(
+#    legend.background = element_rect(color = "transparent", fill = "transparent"),
+#    legend.position.inside = c(.85, .9),
+#    axis.title.y = element_text(size = 11)
+#  )
+
+
+#################################################################################
