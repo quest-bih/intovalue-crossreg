2f13f0db5ed6104e0ef01c793cdb36b2eaf27ba7
Elisa Bascunan <elisa.bascunan@charite.de>
2025-01-10 16:28:19 +0100

    Update on processing script This commit includes the following: - Addition of lubridate package. - Converts dates to Date format at the beginning after loading dataset. - Corrections use NA instead of blank to delete data. - Sensitivity analysis for completion date has a new shorter version.

--- scripts/manual_validation_data_processing.R
+++ scripts/manual_validation_data_processing.R
@@ -1,6 +1,7 @@
 #### MANUAL CHECKS DATA PROCESSING SCRIPT ####
 
 library(readxl)
+library(lubridate)
 library(dplyr)
 library(readr)
 library(stringr)
@@ -21,6 +22,11 @@ data <- read_xlsx(here("data", "manual_validation_raw.xlsx"),
                                 "text", "text", "text", "text", "text",
                                 "logical", "logical", "logical", "logical",
                                 "text"))
+
+# Convert dates to Date format with lubridate()
+data$completion_date_reg1 <- ymd(data$completion_date_reg1)
+data$completion_date_reg2 <- ymd(data$completion_date_reg2)
+data$date_of_check <- ymd(data$date_of_check)
 
 #### RENAMING COLUMNS ####
 
@@ -49,15 +55,15 @@ data[data$trn1 == "2009-017520-88",]$has_summary_results_reg1_main <- FALSE
 data$general_comment[data$trn1 == "2012-004555-36"] <- paste(data$population_comment[data$trn1 == "2012-004555-36"],
                                                              data$general_comment[data$trn1 == "2012-004555-36"])
   # Delete text from population_comment column
-data$population_comment[data$trn1 == "2012-004555-36"] <- ""
+data$population_comment[data$trn1 == "2012-004555-36"] <- NA
 
 # For 2009-014076-22 - NCT01065246, value from general_comment (no DE protocol available) needs to be moved to the correct row: 2012-002699-14 - NCT01883531
 
   # Paste text in the right row
 data$general_comment[data$trn1 == "2012-002699-14"] <- paste(data$general_comment[data$trn1 == "2009-014076-22"],
                                                              data$general_comment[data$trn1 == "2012-002699-14"])
-  # Delete comment from population_comment column 
-data$general_comment[data$trn1 == "2009-014076-22"] <- ""
+  # Delete comment from general_comment column 
+data$general_comment[data$trn1 == "2009-014076-22"] <- NA
 
 #### CREATE ROUNDED COMPLETION DATE COLUMNS FOR DISCREPANCY ANALYSIS ####
 
@@ -255,14 +261,16 @@ if (nrow(non_matching_rows) == 0) {
 
 #### CREATE COLUMN FOR SENSITIVITY ANALYSIS COMPLETION DATE ####
 
-# This is a sensitivity analysis of completion date of EUCTR trn's, based on the one that was present in the German member state protocol instead of the result section.
+# We process the data further to allow for a sensitivity analysis for discrepancies
+# in completion date, where the completion date in EUCTR is only taken from the
+# Germany (DE) country protocol in EUCTR (completion dates from results
+# sections in EUCTR are disregarded; registrations with missing DE country protocols
+# are excluded)
 
 # Load data euctr protocol dataset
-
 euctr_data <- read.csv(here::here("data", "euctr_dump.csv"), sep = ",")
 
 # Check that all trn1 from joined_data are in euctr_data
-
 missing_trn1 <- setdiff(joined_data$trn1, euctr_data$eudract_number)
 
 if (length(missing_trn1) == 0) {
@@ -273,89 +281,57 @@ if (length(missing_trn1) == 0) {
 }
 
 # Filter variables and rows of interest from euctr_data
-
 euctr_data_filtered <- euctr_data |>
   select(eudract_number_with_country, eudract_number, date_of_the_global_end_of_the_trial) |>
   filter(eudract_number %in% joined_data$trn1, 
          str_detect(eudract_number_with_country, "DE"))
 
-# Discrepancy analysis check
-# In the manual_validation_results.qmd file we analyze discrepancies between registries only for manually validated cross-registrations (n = 233)
-# Only 227 observations (out of the 233) are included in euctr_data_filtered: 
+# Convert dates to Date format with lubridate()
+euctr_data_filtered$date_of_the_global_end_of_the_trial <- ymd(euctr_data_filtered$date_of_the_global_end_of_the_trial)
 
-joined_data |>
-  filter(is_true_crossreg == TRUE,
-         trn1 %in% euctr_data_filtered$eudract_number) 
-
-# These 6 observations did not have a DE protocol available:
-
-joined_data |>
-  filter(is_true_crossreg == TRUE,
-         !trn1 %in% euctr_data_filtered$eudract_number) |>
-  select(trn1, general_comment)
-
-# Add data from date_of_the_global_end_of_the_trial to final_data
+# Add completion date from EUCTR DE protocol (date_of_the_global_end_of_the_trial) to joined_data
 joined_data <- joined_data |>
   left_join(euctr_data_filtered |>
               select(eudract_number, 
                      completion_date_protocol = date_of_the_global_end_of_the_trial),
             by = c("trn1" = "eudract_number"))
 
-# Analysis of missing data in completion_date_protocol column for cases with both "Completed" recruitment status
+# Let's explore missing completion dates from the EUCTR dump for this discrepancy analysis
+# The discrepancy analysis of completion date across registrations of the same trial is limited
+# to manually validated cross-registrations where both registrations are "Completed"
 
-joined_data |>
-  filter(overall_recruitment_status_reg1 == "Completed" & overall_recruitment_status_reg2 == "Completed") |>
-  summarise(missing_count = sum(is.na(completion_date_protocol) | completion_date_protocol == ""))
+discrepancy_data <- joined_data |>
+  filter(
+    is_true_crossreg == TRUE,
+    overall_recruitment_status_reg1 == "Completed" & overall_recruitment_status_reg2 == "Completed"
+  )
 
-# 28 rows have missing data:
+# There are 200 observations in the discrepancy analysis of completion date
+# This includes 28 observations with a missing completion_date_protocol from the EUCTR dump
+discrepancy_data |>
+  count(is.na(completion_date_protocol))
 
-joined_data |>
+# Let's explore missing the 28 completion_date_protocol data in the discrepancy data
+#   - 6 of these do not have a DE protocol in the EUCTR dump
+#   - 8 of these do not have a completion date on either the DE protocol or results (see `completion_date_type_reg1` == Missing)
+#   - 14 of these do not have a completion date on the DE protocol
+
+discrepancy_data |>
   select(trn1, 
          general_comment,
-         overall_recruitment_status_reg1, 
-         overall_recruitment_status_reg2,
+         completion_date_reg1,
+         completion_date_type_reg1,
          completion_date_protocol) |>
-  filter(overall_recruitment_status_reg1 == "Completed" & overall_recruitment_status_reg2 == "Completed") |>
-  filter(is.na(completion_date_protocol) | completion_date_protocol == "") |>
+  filter(is.na(completion_date_protocol)) |>
+  arrange(general_comment, desc(completion_date_type_reg1)) |>
   print(n = Inf)
 
-# The 6 NA in the completion_date_protocol column are the rows that do not have a DE protocol.
-# The remaining 22 rows are blank. Upon inspecting them on EUCTR website I found that in protocol they have completed status and NO global end of trial. 
-# If I clicked results, then there was always a date available there, which could explain why in main analysis we did not encounter this issue for 
-# overall enrollment complete trials in more than 8 cases. 
-
-# Review of completion_date_reg1 missing data for cases with both "Completed" recruitment status:
-
-joined_data |>
-  mutate(completion_date_reg1 = as.character(completion_date_reg1)) |>
-  select(trn1, 
-         general_comment,
-         overall_recruitment_status_reg1, 
-         overall_recruitment_status_reg2,
-         completion_date_reg1) |>
-  filter(overall_recruitment_status_reg1 == "Completed" & overall_recruitment_status_reg2 == "Completed") |>
-  filter(is.na(completion_date_reg1) | completion_date_reg1 == "" | completion_date_reg1 == "N/A") |>
-  print(n = Inf)
-
-# There are 8 cases of missing completion date, but complete recruitment status.
-
-# Upon manually checking the 28 missing from euctr_data, I verified that each euctr trn that had no date on both protocol and results, was also labelled 
-# as N/A in the manually extracted completion date. Therefore, of the 28 missing from Nick's data, 6 are not DE protocol, 14 are missing completion date 
-# on the protocol, and 8 are missing their completion date on both protocol and results section in EUCTR registry page.
-
-# Create rounded completion date column for completion_date_protocol
-
-class(joined_data$completion_date_protocol)   # Check type of data for completion_date_protocol - it is a character. It needs to be "POSIXct" "POSIXt" 
-
-joined_data$completion_date_protocol <- as.POSIXct(joined_data$completion_date_protocol, format = "%Y-%m-%d") # Convert to desired class
-
-# Now we can round the completion_date_protocol column to year-month:
-
+# Round the completion_date_protocol column:
 joined_data <- joined_data |> 
   mutate(
-    completion_date_protocol = lubridate::floor_date(completion_date_protocol, unit = "month"),
+    completion_month_year_protocol = lubridate::floor_date(completion_date_protocol, unit = "month"),
   ) |> 
-  relocate(completion_date_protocol, .after = completion_month_year_reg1)
+  relocate(completion_month_year_protocol, .after = completion_month_year_reg1)
 
 ### EXPORT FINAL DATASET
 
