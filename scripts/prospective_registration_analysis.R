3cffaab165899c1e46881264912c5c4d2e9fd2ce
Delwen Franzen <delwen@franzen.fm>
2025-02-21 19:35:52 +0100

    Refactored prospective registration code
    Separated out the DRKS download

--- scripts/prospective_registration_analysis.R
+++ scripts/prospective_registration_analysis.R
@@ -4,11 +4,9 @@
 
 library(readr)
 library(dplyr)
-library(purrr)
 library(here)
 library(stringr)
 library(ggplot2)
-library(dRks)
 library(lubridate)
 
 # Load in list of screened trial pairs to limit analysis to manually confirmed crossreg
@@ -16,36 +14,14 @@ manual_screening <- read_csv(here("data", "manual_validation_processed.csv"))
 manual_confirmed <- manual_screening |>
   filter(is_true_crossreg)
 
-# DRKS
-##### load IntoValue dataset from OSF and pull DRKS trial IDs
+drks_prereg_info <- read_csv(here("data", "prereg_processed", "iv_drks_prereg.csv"))
 
-drks_ids <- readr::read_csv("https://osf.io/download/mkgux") |>
-  filter(registry == "DRKS") |>
-  
-  # filter out invalid DRKS IDs that no longer resolve in the registry (n=3)
-  filter(id != "DRKS00004097", id != "DRKS00003807", id != "DRKS00004108") |>
-  pull(id)
-
-slow_download_drks <- slowly(download_drks, rate = rate_delay(1)) # this delay worked for me without issues
-
-##### assuming you want to download the html to a "raw" subfolder, change path if needed
-
-drks_ids |>
-  walk(\(x) slow_download_drks(x, dir = here("data", "prereg_raw")))
-
-drks_prereg_info <- map(drks_ids, \(x) parse_drks_study(filepath = file.path(here("data", "prereg_raw"), x)), .progress = TRUE)
-
+# Convert to lubridate date format in case of further processing of dates
 drks_prereg_info <- drks_prereg_info |>
-  list_rbind() |>
-  mutate(start_date = ymd(start_date),
-         registration_date = ymd(registration_date),
-         has_prospective_registration = floor_date(start_date, unit = "month") >=
-           floor_date(registration_date, unit = "month")) |>
-  rename(id = drks_id) |>
-  select(id, registration_date, start_date, has_prospective_registration)
-
-drks_prereg_info |>
-  write_csv(here("data", "prereg_processed", "iv_drks_prereg.csv"))
+  mutate(
+    start_date = ymd(start_date),
+    registration_date = ymd(registration_date)
+    )
 
 # CLinicalTrials.gov
 # Data was downloaded from AACT (20240927) and processed to obtain prospective registration
@@ -54,7 +30,12 @@ drks_prereg_info |>
 
 ctgov_prereg_info <- read_csv(here("data", "prereg_processed", "iv_ctgov_prereg.csv")) |>
   rename(id = nct_id, registration_date = study_first_submitted_date) |>
-  select(id, registration_date, start_date, has_prospective_registration)
+  select(id, registration_date, start_date, has_prospective_registration) |>
+  # Convert to lubridate date format in case of further processing of dates
+  mutate(
+    start_date = ymd(start_date),
+    registration_date = ymd(registration_date)
+  )
 
 # Combine DRKS and ctgov data
 prereg_combined <- bind_rows(drks_prereg_info, ctgov_prereg_info) |>
